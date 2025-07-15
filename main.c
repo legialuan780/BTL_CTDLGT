4748564b972985839d90dfa09a9195f86fdda01f
Le Gia Luan <legialuan780@gmail.com>
2025-07-16 00:42:02 +0700

    them mo ta cho struct Dictionary

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 struct Dictionary{
-	char en[100];
-	char vi[100];
-	int order; 
+	char en[100];// tieng anh
+	char vi[100];// nghia tieng viet
+	int order; // so thu tu  
 };
 
 typedef struct node
