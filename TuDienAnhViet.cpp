719b58362c4bfc15d105b2bbe56744c5253daf0d
Le Gia Luan <legialuan780@gmail.com>
2025-07-25 01:27:24 +0700

    them mau ham sua xoa

--- TuDienAnhViet.cpp
+++ TuDienAnhViet.cpp
@@ -1,66 +1,71 @@
 #include <iostream>
 using namespace std;
 
-typedef struct word {
-    string en;
-    string vi;
-    int id;
+typedef struct word
+{
+  string en;
+  string vi;
+  int id;
 } Word;
 
-int currentID = 0; 
+int currentID = 0;
 
-void inputWord(Word &w) {
-    cin.ignore(); 
-    cout << "Nhap tu tieng Anh: ";
-    getline(cin, w.en);
-    cout << "Nhap nghia tieng Viet: ";
-    getline(cin, w.vi);
-    w.id = ++currentID;
+void inputWord(Word &w)
+{
+  cin.ignore();
+  cout << "Nhap tu tieng Anh: ";
+  getline(cin, w.en);
+  cout << "Nhap nghia tieng Viet: ";
+  getline(cin, w.vi);
+  w.id = ++currentID;
 }
 
-typedef struct node {
-    struct node *prev;
-    Word data;
-    struct node *next;
+typedef struct node
+{
+  struct node *prev;
+  Word data;
+  struct node *next;
 } Node;
 
-typedef struct dlist {
-    Node* head;
-    Node* tail;
-    int size;
-    dlist();
-    Node *createNode(Word &x);
-    void addWord(Word &x); // them rang buoc khi trung tu
-    void search(string word);
-    void display();
-    void prevWord(Word &x);
-    void sizeDict();
-    void removeWord(string word);
-    void editWord(string word, Word &newWord);
-    void swap(Node *&a, Node *&b);
-    Node *getlastNode();
-    Node *partition(Node *&low, Node *&high);
-    void quickSort(Node *&low, Node *&high);
+typedef struct dlist
+{
+  Node *head;
+  Node *tail;
+  int size;
+  dlist();
+  Node *createNode(Word &x);
+  void addWord(Word &x); // them rang buoc khi trung tu
+  void search(string word);
+  void display();
+  void prevWord(Word &x);
+  void sizeDict();
+  void removeWord(string word);
+  void editWord(string word, Word &newWord);
+  void swap(Node *&a, Node *&b);
+  Node *getlastNode();
+  Node *partition(Node *&low, Node *&high);
+  void quickSort(Node *&low, Node *&high);
 
 } Dlist;
 
-dlist::dlist() {
-    head = tail = NULL;
-    size = 0;
+dlist::dlist()
+{
+  head = tail = NULL;
+  size = 0;
 }
 
-Node *dlist::createNode(Word &x) {
-    Node *p = new Node;
-    p->data = x;
-    p->prev = NULL;
-    p->next = NULL;
-    return p;
+Node *dlist::createNode(Word &x)
+{
+  Node *p = new Node;
+  p->data = x;
+  p->prev = NULL;
+  p->next = NULL;
+  return p;
 }
-
 
 void dlist::addWord(Word &x)
 {
-   Node *a = createNode(x);
+  Node *a = createNode(x);
   if (a == NULL)
   {
     cout << "het bo nho";
@@ -77,7 +82,6 @@ void dlist::addWord(Word &x)
     tail = a;
     size++;
   }
-
 }
 
 void dlist::search(string word)
@@ -89,17 +93,17 @@ void dlist::search(string word)
     if (p->data.en == word)
     {
       cout << "Tim thay tu: " << p->data.en << " - " << p->data.vi << " - id " << p->data.id << endl;
-      if(p == head) // Neu node da o dau danh sach
+      if (p == head) // Neu node da o dau danh sach
       {
         return; // Khong can di chuyen
       }
       q->next = p->next; // Xoa node da tim thay khoi danh sach
-      p->next=head; // Di chuyen node da tim thay ve dau
-      p->prev=NULL;
-      head=p;
+      p->next = head;    // Di chuyen node da tim thay ve dau
+      p->prev = NULL;
+      head = p;
       return;
     }
-    q=p;
+    q = p;
     p = p->next;
   }
   cout << "Khong tim thay tu" << endl;
@@ -116,13 +120,12 @@ void dlist::swap(Node *&a, Node *&b)
 Node *dlist::getlastNode()
 {
   Node *p = head;
-  while (head!=NULL && p->next != NULL)
+  while (head != NULL && p->next != NULL)
   {
     p = p->next;
   }
   return p;
 }
-
 
 Node *dlist::partition(Node *&low, Node *&high)
 {
@@ -144,25 +147,22 @@ Node *dlist::partition(Node *&low, Node *&high)
 
 void dlist::quickSort(Node *&low, Node *&high)
 {
-   if (low != nullptr && high != nullptr 
-        && low != high && low != high->next) {
-      
-        // Find the partition node (pivot)
-        Node *pivot = partition(low, high);
+  if (low != nullptr && high != nullptr && low != high && low != high->next)
+  {
 
-        // Recursively sort the left half
-        quickSort(low, pivot->prev);
+    Node *pivot = partition(low, high);
 
-        // Recursively sort the right half
-        quickSort(pivot->next, high);
-    }
+    quickSort(low, pivot->prev);
+
+    quickSort(pivot->next, high);
+  }
 }
 
 void dlist::display()
 {
   for (Node *i = head; i != NULL; i = i->next)
   {
-    cout << "(" << i->data.id << ") " << i->data.en << " - " << i->data.vi << " "<<endl;
+    cout << "(" << i->data.id << ") " << i->data.en << " - " << i->data.vi << " " << endl;
   }
   if (head == NULL)
   {
@@ -172,28 +172,30 @@ void dlist::display()
 
 int main()
 {
-    Word w;
-    Dlist ds;
-    cout << "Nhap so luong tu: ";
-    int n;
-    cin >> n;
-    cout << "Nhap lan luot tu tieng Anh va nghia tieng Viet:\n";
-    for (int i = 0; i < n; i++) {
-        inputWord(w);
-        ds.addWord(w);
-    }
-    cout << "Danh sach tu dien:\n";
-    Node *lastNode = ds.getlastNode();
-    ds.quickSort(ds.head, lastNode);
-    ds.display();
-    // cout << "Nhap tu can tim: ";
-    // string word;
-    // getline(cin, word);
-    // ds.search(word);
-    //  ds.display();
-    // cout << "Nhap tu can tim: ";
-    // string word2;
-    // getline(cin, word2);
-    // ds.search(word2);
-    return 0;
+  Word w;
+  Dlist ds;
+  cout << "Nhap so luong tu: ";
+  int n;
+  cin >> n;
+  cout << "Nhap lan luot tu tieng Anh va nghia tieng Viet:\n";
+  for (int i = 0; i < n; i++)
+  {
+    inputWord(w);
+    ds.addWord(w);
+  }
+
+  cout << "Danh sach tu dien:\n";
+  Node *lastNode = ds.getlastNode();
+  ds.quickSort(ds.head, lastNode);
+  ds.display();
+  cout << "Nhap tu can tim: ";
+  string word;
+  getline(cin, word);
+  ds.search(word);
+  ds.display();
+  cout << "Nhap tu can tim: ";
+  string word2;
+  getline(cin, word2);
+  ds.search(word2);
+  return 0;
 }
